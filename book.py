71023b60faecf53aab1ae150c137e7ae6e22abc4
Dennis <76845672+DennisNeu@users.noreply.github.com>
2024-10-31 08:40:31 +0100

    add book class, begun work on library class

---
+++ book.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.is_borrowed = False
+    
+    def __str__(self):
+        print(f"title: '{self.title}' author '{self.author}'")
+    
